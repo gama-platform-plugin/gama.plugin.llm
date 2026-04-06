0f25d61f1eb75c590fcb63717879d9238bd74b6f
RoiArthurB <contact@arthurbrugiere.fr>
2026-04-07 01:55:16 +0700

    fixup! fix: GHA point to renamed maven parent folder

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ else
     exit 1
 fi
 
-cd "${ROOT}/parent"
+cd "${ROOT}/gama.plugin.parent"
 mvn clean install -B \
     -Dgama.p2.version="${GAMA_P2_VERSION}" \
     -Ddeploy.subdir="${PLUGIN_REPO_NAME}" \
